5fcce3949d461056a867ccef6069c5f70d9cc69f
Sally <causeysally@gmail.com>
2025-12-18 23:11:32 -0600

    addded new adventurer and healing and damage points

--- src/games/game-1.js
+++ src/games/game-1.js
@@ -1,5 +1,15 @@
 import { Adventurer } from "../models/Adventurer.js";
 console.log("Fantasy Forge initialized!");
 const hero1 = new Adventurer("Robin", 80);
+const hero2 = new Adventurer("Lurona", 60);
+//robin//
+hero1.receiveDamage(20);
+hero1.heal(15);
+//Lurona//
+hero2.receiveDamage(10);
+hero2.heal(25);
+//adventurer health record//
+console.log(`${hero1.name}'s final health: ${hero1.health}`);
+console.log(`${hero2.name}'s final health: ${hero2.health}`);
 console.log(" First Adventurer person");
 hero1.scout();
