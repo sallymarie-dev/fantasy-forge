2f86703b0b65e887c191dca7e25db1edb76299e9
Sally <causeysally@gmail.com>
2025-12-18 22:47:11 -0600

    first person created

--- src/games/game-1.js
+++ src/games/game-1.js
@@ -1,4 +1,5 @@
-import Adventurer from "../models/Adventurer.js";
+import { Adventurer } from "../models/Adventurer.js";
 console.log("Fantasy Forge initialized!");
 const hero1 = new Adventurer("Robin", 80);
+console.log(" First Adventurer person");
 hero1.scout();
