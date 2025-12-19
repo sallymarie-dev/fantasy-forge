90b9404d57f7d112a3ae8a769fec24ab09fed6a4
Sally <causeysally@gmail.com>
2025-12-18 22:26:39 -0600

    added to adventurer

---
+++ src/models/Adventure.js
@@ -0,0 +1,10 @@
+class Adventurer {
+  constructor(name, health) {
+    this.name = name;
+    this.health = health;
+  }
+  scout() {
+    console.log(`${this.name} is scouting.... Health: ${this.health}`);
+  }
+}
+export default Adventurer;
