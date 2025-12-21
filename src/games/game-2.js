574ff8600ae5fec8c69bedea385660ffdca1257c
Sally <causeysally@gmail.com>
2025-12-21 14:03:41 -0600

    added magical irems

--- src/games/game-2.js
+++ src/games/game-2.js
@@ -1,5 +1,6 @@
 import Adventurer from "../models/Adventurer.js";
 import HealPotion from "../models/HealPotion.js";
+import MagicItem from "../models/MagicItem.js";
 
 console.log("Game 2 initialized!");
 
@@ -8,6 +9,11 @@ const hero = new Adventurer("Nolora", 50);
 
 // Creating potion
 const potion = new HealPotion("Minor Heal Potion", 30, 2);
+const invisibilityCloak = new MagicItem(
+  "Invisibility Cloak",
+  "Grants and stabalize invisibility",
+  1
+);
 
 // Damage to the adventurer
 hero.receiveDamage(20);
@@ -17,3 +23,20 @@ potion.use(hero);
 
 //  no charges left but use anyway
 potion.use(hero);
+// confirming depletion
+potion.use(hero);
+
+// use the cloak & confirm
+invisibilityCloak.use(hero);
+invisibilityCloak.use(hero);
+ 
+//rcharging//
+potion.charges += 2; 
+console.log(`${potion.name} recharged. Charges now: ${potion.charges}`);
+//use them again if wants//
+potion.use(hero);
+invisibilityCloak.use(hero);
+
+console.log("\n--- Final Item Charges ---");
+console.log(`${potion.name}: ${potion.charges}`);
+console.log(`${invisibilityCloak.name}: ${invisibilityCloak.charges}`);
