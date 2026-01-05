3593a492a9bd38e0d7dc361116c2ba5142ef20be
Sally <causeysally@gmail.com>
2026-01-04 18:35:24 -0600

    updated healpotions

--- src/models/HealPotion.ts
+++ src/models/HealPotion.ts
@@ -1,16 +1,24 @@
-class HealPotion {
-  constructor(name, healAmount, charges) {
+import  Adventurer from './Adventurer';
+export class HealPotion {
+  name:string;
+  healAmount:number;
+  charges:number;
+
+  constructor(name:string, healAmount:number, charges:number) {
     this.name = name;
     this.healAmount = healAmount;
     this.charges = charges;
   }
-  use(target) {
+
+  use(target:Adventurer) {
     if (this.charges <= 0) {
       console.log(`${this.name} has no charges left.`);
       return;
     }
     target.health += this.healAmount;
+
     this.charges--;
+
     console.log(
       `${target.name} used ${this.name} and heal ${this.healAmount}HP. Health is now ${target.health}. Has charges left: ${this.charges} `
     );
